b0888fe24c08e64fadc11d9fe866f991ab2e7e4d
“maxoilmanwilliams” <maxow@umich.edu>
2017-11-10 14:50:52 -0500

    User Checkin Class

--- app/src/main/java/com/to426project/happyhourapp/UserCheckIn.java
+++ app/src/main/java/com/to426project/happyhourapp/UserCheckIn.java
@@ -8,6 +8,24 @@ import android.location.Location;
 
 public class UserCheckIn {
 
+    public String Location;
+    public String Username;
+    public String UID;
+    public String Restaurant;
+
+    public UserCheckIn(String Location,
+            String Username,
+            String UID,
+            String Restaurant){
+        this.Location = Location;
+        this.Restaurant=Restaurant;
+        this.Username = Username;
+        this.UID = UID;
+    }
+
+    public UserCheckIn(){
+
+    }
     /* Time of check-in    Date/time format
     Location            Coordinates
     User                Username

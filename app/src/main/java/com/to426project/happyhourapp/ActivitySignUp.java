fc11f885e683390953e326342f62e0b50f36c2ed
“maxoilmanwilliams” <maxow@umich.edu>
2017-11-12 12:33:42 -0500

    Fix menus

---
+++ app/src/main/java/com/to426project/happyhourapp/ActivitySignUp.java
@@ -0,0 +1,13 @@
+package com.to426project.happyhourapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivitySignUp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
